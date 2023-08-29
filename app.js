ab7de97b00e9b759f8c94835c52de46e30a49089
Assignment-JS-1-to-40-Chp <aryanmali000@gmail.com>
2023-08-30 02:01:31 +0500

    CountdownTimer Assignment Done

---
+++ app.js
@@ -0,0 +1,28 @@
+let endDate = "26 sep 2023 11:00 PM"
+
+document.getElementById("end-date").innerHTML = endDate;
+
+let inputs = document.querySelectorAll("input")
+
+
+function clock() {
+    let end = new Date(endDate);
+    let now = new Date()
+    let diff = (end - now) / 1000;
+    // console.log(diff);
+
+    if (diff < 0) return;
+    inputs[0].value = (Math.floor(diff / 3600 / 24));
+    inputs[1].value = (Math.floor(diff / 3600) % 24);
+    inputs[2].value = (Math.floor(diff / 60) % 60);
+    inputs[3].value = (Math.floor(diff) % 60);
+
+}
+clock()
+
+setInterval(
+    () => {
+        clock()
+    },
+    1000
+)
